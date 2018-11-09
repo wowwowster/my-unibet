bf118b3c9377b37b4e8402b9f886c5ee2d178132
CARL <fathera6@gmail.com>
2018-11-09 11:52:16 +0100

    Initial commit

---
+++ src/main/java/com/claurier/myunibet/MyUnibetApplication.java
@@ -0,0 +1,12 @@
+package com.claurier.myunibet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUnibetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUnibetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/claurier/myunibet/MyUnibetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.claurier.myunibet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyUnibetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
